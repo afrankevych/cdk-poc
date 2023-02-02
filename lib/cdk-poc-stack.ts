7f3bd9d2470135b4eb5bbca5a496d44059361701
Frankevych <andriy.frankevych@docebo.com>
2023-02-02 11:24:44 +0100

    add s3 bucket to app stack

--- lib/cdk-poc-stack.ts
+++ lib/cdk-poc-stack.ts
@@ -1,16 +1,15 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { aws_s3 as S3 } from 'aws-cdk-lib';
 
 export class CdkPocStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkPocQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    
+    new S3.Bucket(this, 'CDKPOCBucket', {
+      versioned: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    })
   }
 }
