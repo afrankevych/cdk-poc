8514cb4dfdf98fc2c3ed19928584a84e0790ed2e
Frankevych <andriy.frankevych@docebo.com>
2023-02-03 12:45:56 +0100

    add api gateway

--- lib/cdk-poc-stack.ts
+++ lib/cdk-poc-stack.ts
@@ -1,15 +1,20 @@
 import * as cdk from 'aws-cdk-lib';
 import {Construct} from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda'
+import * as apigw from 'aws-cdk-lib/aws-apigateway'
 
 export class CdkPocStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
-        new lambda.Function(this, 'mock-handler', {
+        const mockHandler = new lambda.Function(this, 'mock-handler', {
             runtime: lambda.Runtime.NODEJS_18_X,
             code: lambda.Code.fromAsset('dist/lambda'),
             handler: 'mock-handler.handler'
         });
+
+        new apigw.LambdaRestApi(this, 'mock-gateway', {
+            handler: mockHandler
+        })
     }
 }
