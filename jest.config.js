5b51ba7a2b3de03f7b4b0c3a1ed13fc257d27fde
Frankevych <andriy.frankevych@docebo.com>
2023-02-06 09:26:39 +0100

    update jest conf to include all files to coverage report

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,19 @@
 module.exports = {
   testEnvironment: 'node',
-  roots: ['<rootDir>/test'],
+  roots: [
+      '<rootDir>/lib',
+      '<rootDir>/lambda',
+      '<rootDir>/src',
+      '<rootDir>/test'
+  ],
   testMatch: ['**/*.test.ts'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest'
   },
+  collectCoverageFrom: [
+    '**/*.{ts,jx}',
+    '!dist/*',
+    '!node_modules/*',
+  ],
   coverageReporters: ['text', 'lcov'],
 };
